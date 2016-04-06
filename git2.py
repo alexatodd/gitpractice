1e26c69a17d4fe92be6548d590bd6acdb53c45ba
anorthfox <alexatodd@gmail.com>
2016-04-05 18:59:59 -0700

    added more notes about checkout

---
+++ git2.py
@@ -0,0 +1,15 @@
+# Notes from PyLadies Git Workshop 2: branching
+
+branch = "git branch new_branch" 
+# -d to delete with verification, -D delete no questions
+# -m|-M to rename the branch ()
+checkout = "git checkout new_branch" # how you move around in the git tree
+checkout_master = "to get back to the master"
+
+HEAD = "the commit that you are working from"
+
+git_log = "everything in parent's lineage"
+git
+
+# o->o->o
+#    |->o
